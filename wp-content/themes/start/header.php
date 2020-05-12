2572fc234d1d8a8004fb7c13ccaace2c73dc007f
omoterikuto <omoterikuto@gmail.com>
2020-05-12 21:49:28 +0900

    5/12

--- wp-content/themes/start/header.php
+++ wp-content/themes/start/header.php
@@ -1,45 +1,45 @@
 <?php
 
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link href="https://fonts.googleapis.com/css?family=Raleway|Roboto:300&display=swap" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP|Raleway&display=swap" rel="stylesheet">
-	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
-	<link rel="pingback" href="<?php echo esc_url( get_bloginfo( 'pingback_url' ) ); ?>">
-	<?php endif; ?>
-	<?php wp_head(); ?>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="profile" href="http://gmpg.org/xfn/11">
+  <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto:300&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP|Raleway&display=swap" rel="stylesheet">
+  <?php if (is_singular() && pings_open(get_queried_object())) : ?>
+    <link rel="pingback" href="<?php echo esc_url(get_bloginfo('pingback_url')); ?>">
+  <?php endif; ?>
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
+  <?php wp_body_open(); ?>
 
 
-<header id="masthead" class="site-header" role="banner">
-	<div class="head-bar">
-		<div id="hamburger-menu" class="none">
-			<?php wp_nav_menu( array('theme_location' => 'global' )); ?>
-		</div>
-		<div class="head-bar-contents">
-			<div class="logo">
-				<h4>RoimBlog</h4>
-			</div>
-			<div id="toggle">
-				<span></span>
-				<span></span>
-				<span></span>
-			</div>
-		</div>
-	</div>
+  <header id="masthead" class="site-header" role="banner">
+    <div class="head-bar">
+      <div class="head-bar-contents">
+        <div class="logo">
+          <h4>StartUp</h4>
+        </div>
+        <div id="hamburger-menu" class="none">
+          <?php wp_nav_menu(array('theme_location' => 'global')); ?>
+        </div>
+        <div id="toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div id="header-menu">
+        <?php wp_nav_menu(array('theme_location' => 'global')); ?>
+      </div>
+    </div>
 
-	<div class="site-branding">
-		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-	</div><!-- .site-branding -->
-	<div id="header-menu" class="header-menu">
-		<?php wp_nav_menu( array('theme_location' => 'global' )); ?>
-	</div>
-</header><!-- .site-header -->
+
+  </header><!-- .site-header -->
--- wp-content/themes/start/startup_guide.php
+++ wp-content/themes/start/startup_guide.php
@@ -1,27 +1,13 @@
 <?php
 
 /**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package WordPress
- * @since Twenty Sixteen 1.0
+ *Template Name: startup_guide
  */
 
 get_header(); ?>
-<div class="site-branding">
-  <h1 class="site-title">小さく、大きな<br>はじめの一歩を</h1>
-</div><!-- .site-branding -->
-<div class="top-contents">
+<div class="contents">
 
   <main id="main">
-    <?php get_template_part('pickup'); ?>
     <div id="primary" class="content-area">
       <div class="page-header">
         <h2>最新記事</h2>
